64ba7c4b91d36fe2493ceacc3cb7aed034cc0a91
Mohammed Islamuddin Siddiqui <islam@Mohammeds-Mac-mini.local>
2025-11-16 19:58:01 +0530

    Minor Changes

--- src/main/java/com/islam/rag_demo/DocumentLoader.java
+++ src/main/java/com/islam/rag_demo/DocumentLoader.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Component;
 public class DocumentLoader implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(DocumentLoader.class);
-    @Value("classpath:/pdf/System_Design_on_AWS.pdf")
+    @Value("classpath:/pdf/spring-boot-reference.pdf")
     private Resource resource;
     private final VectorStore vectorStore;
 
@@ -31,17 +31,5 @@ public class DocumentLoader implements CommandLineRunner {
         log.info("Ingesting PDF file" + resource.getFilename());
         vectorStore.accept(textSplitter.split(reader.read()));
         log.info("Completed Ingesting PDF file");
-
-
-        // List<Document> documents = List.of(
-        //     new Document("StarlightDB is a serverless graph database designed for real-time analytics on complex, interconnected data."),
-        //     new Document("The core of StarlightDB is its 'Quantum-Leap' query engine, which uses speculative execution to deliver query results up to 100x faster than traditional graph databases."),
-        //     new Document("StarlightDB features 'Chrono-Sync' for effortless time-travel queries, allowing developers to query the state of their graph at any point in the past."),
-        //     new Document("StarlightDB includes a built-in visualization tool called 'Nebula' that renders interactive 3D graphs directly within the development environment for easier analysis."),
-        //     new Document("Security in StarlightDB is handled by 'Cosmic Shield', which provides end-to-end encryption and fine-grained access control at the node and edge level.")
-        // );
-        // System.out.println("Starting to load Documents into VectorStore.");
-        // vectorStore.add(documents);
-        // System.out.println("Documents loaded into VectorStore.");
     }
 }
